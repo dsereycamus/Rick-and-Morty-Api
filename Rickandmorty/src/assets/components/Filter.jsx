480b7d153c126bf8fb0af6706b9585c92526cf8e
Daniela Serey <dsereycamus@gmail.com>
2024-01-16 13:45:42 -0300

    Modificando filtros

--- Rickandmorty/src/assets/components/Filter.jsx
+++ Rickandmorty/src/assets/components/Filter.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
 import Form from "react-bootstrap/Form";
 
-function Filter({ onStatusChange }) {
+function Filter({ onStatusChange, onSortChange }) {
+  const [selectedStatus, setSelectedStatus] = useState("");
+  const [selectedSort, setSelectedSort] = useState("asc");
+
+  const handleStatusChange = (e) => {
+    const status = e.target.value;
+    setSelectedStatus(status);
+    onStatusChange(status);
+  };
+
+  const handleSortChange = (e) => {
+    const sortDirection = e.target.value;
+    setSelectedSort(sortDirection);
+    onSortChange(sortDirection);
+  };
+
   return (
-    <Form.Select aria-label onChange={(e) => onStatusChange(e.target.value)}>
-      <option>Filter status</option>
-      <option value="alive">Alive</option>
-      <option value="dead">Dead</option>
-      <option value="unknown">Unknown</option>
-    </Form.Select>
+    <div className="my-4">
+      <div className="input-group">
+        <Form.Select
+          aria-label="Default select example"
+          value={selectedStatus}
+          onChange={handleStatusChange}
+          className="custom-select"
+        >
+          <option>Filter by Status</option>
+          <option value="alive">Alive</option>
+          <option value="dead">Dead</option>
+          <option value="unknown">Unknown</option>
+        </Form.Select>
+
+        <Form.Select
+          aria-label="Default select example"
+          value={selectedSort}
+          onChange={handleSortChange}
+          className="custom-select ml-2"
+        >
+          <option>Alphabetical order</option>
+          <option value="asc">A to Z</option>
+          <option value="desc">Z to A</option>
+        </Form.Select>
+      </div>
+    </div>
   );
 }
 
--- Rickandmorty/src/assets/components/MiApi.jsx
+++ Rickandmorty/src/assets/components/MiApi.jsx
@@ -1,26 +1,35 @@
+// Importaciones de React y componentes necesarios
 import { useState, useEffect } from "react";
 import { Character } from "./Character";
 import Buscador from "./Buscador";
 import MyPagination from "./Pagination";
 import Filter from "./Filter";
 
+// Definición del componente principal
 export function MiApi() {
-  const [loading, setLoading] = useState(true);
-  const [characters, setCharacters] = useState([]);
-  const [maxPages, setMaxPages] = useState(0);
-  const [page, setPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [error, setError] = useState("");
-  const [status, setStatus] = useState("");
+  // Estados del componente
+  const [loading, setLoading] = useState(true); // Estado de carga
+  const [characters, setCharacters] = useState([]); // Lista de personajes
+  const [maxPages, setMaxPages] = useState(0); // Número máximo de páginas
+  const [page, setPage] = useState(1); // Página actual
+  const [searchTerm, setSearchTerm] = useState(""); // Término de búsqueda
+  const [error, setError] = useState(""); // Mensaje de error
+  const [status, setStatus] = useState(""); // Estado de los personajes
+  const [sortDirection, setSortDirection] = useState("asc"); // Dirección de ordenamiento
 
+  // useEffect para cargar datos de la API
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
+
+      // Construcción de la URL de la API con parámetros de búsqueda, paginación y ordenamiento
       const data = await fetch(
         `https://rickandmortyapi.com/api/character?page=${page}${
           searchTerm !== "" ? `&name=${searchTerm}` : ""
         }${status !== "" ? `&status=${status}` : ""}`
       );
+
+      // Manejo de errores (404)
       if (data.status === 404) {
         setCharacters([]);
         setMaxPages(0);
@@ -28,40 +37,61 @@ export function MiApi() {
         setLoading(false);
         return;
       }
+
+      // Extracción de resultados y metadata de la respuesta
       const { results, info } = await data.json();
+
+      // Configuración de estados con los datos obtenidos y ordenamiento
       setMaxPages(info.pages);
       setCharacters(
         results.sort(function (a, b) {
-          if (a.name > b.name) {
-            return 1;
+          if (sortDirection === "asc") {
+            return a.name.localeCompare(b.name);
+          } else {
+            return b.name.localeCompare(a.name);
           }
-          if (a.name < b.name) {
-            return -1;
-          }
-          return 0;
         })
       );
+
       setLoading(false);
     }
-    fetchData();
-  }, [page, searchTerm, status]);
 
+    // Llamada a la función de carga de datos
+    fetchData();
+  }, [page, searchTerm, status, sortDirection]);
+
+  // Función para manejar el cambio en el término de búsqueda
   const handleSearch = (term) => {
     setSearchTerm(term);
-    setPage(1);
+    setPage(1); // Reiniciar la página a 1 al realizar una nueva búsqueda
   };
 
+  // Función para manejar el cambio en la dirección de ordenamiento
+  const handleSortChange = (direction) => {
+    setSortDirection(direction);
+  };
+
+  // Renderizado del componente
   return (
     <div className="container">
+      {/* Componente de búsqueda */}
       <Buscador onSearch={handleSearch} />
-      <Filter onStatusChange={(value) => setStatus(value)} />
 
+      {/* Componente de filtro */}
+      <Filter
+        onStatusChange={(value) => setStatus(value)}
+        onSortChange={handleSortChange}
+      />
+
+      {/* Condicional de carga: Muestra mensaje de carga o lista de personajes */}
       {loading ? (
         <div>Loading...</div>
       ) : (
         <div className="row">
+          {/* Mapeo de la lista de personajes */}
           {characters.map((character) => (
             <div className="col-md-4 p-4" key={character.id}>
+              {/* Componente individual de personaje */}
               <Character
                 key={character.id}
                 name={character.name}
@@ -73,12 +103,15 @@ export function MiApi() {
               />
             </div>
           ))}
+          {/* Mostrar mensaje de error si hay un error */}
           {error && <div>{error}</div>}
         </div>
       )}
 
+      {/* Mostrar paginación si no hay carga y hay más de una página */}
       {!loading && maxPages > 0 && (
         <div className="d-flex justify-content-center p-3">
+          {/* Componente de paginación */}
           <MyPagination page={page} setPage={setPage} maxPages={maxPages} />
         </div>
       )}
@@ -86,4 +119,5 @@ export function MiApi() {
   );
 }
 
+// Exportar el componente principal
 export default MiApi;
